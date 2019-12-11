34785c72f3a2e2b5a07f6e5bffb6f92278098da4
leehour <862215949@qq.com>
2019-12-12 07:59:16 +0800

    添加二叉搜索树与双向链表题

---
+++ python/二叉搜索树与双向链表.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def Convert(self, pRootOfTree):
+        # write code here
+        if not pRootOfTree:
+            return None
+
+        if not pRootOfTree.left and not pRootOfTree.right:
+            return pRootOfTree
+
+        left = self.Convert(pRootOfTree.left)
+
+        p = left
+
+        while left and p.right:
+            p = p.right
+
+        if left:
+            p.right = pRootOfTree
+            pRootOfTree.left = p
+
+        right = self.Convert(pRootOfTree.right)
+
+        if right:
+            right.left = pRootOfTree
+            pRootOfTree.right = right
+        return left if left else pRootOfTree
+
