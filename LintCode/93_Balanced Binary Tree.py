80f79b064cfcfe3cc78fbf36f27d62168e01f70e
lihao <862215949@qq.com>
2020-04-02 08:13:15 +0800

    添加LintCode第93题：Balanced Binary Tree

--- LintCode/93_Balanced Binary Tree.py
+++ LintCode/93_Balanced Binary Tree.py
@@ -38,3 +38,23 @@ class Solution:
             return ReturnType(False, -1)
 
         return ReturnType(True, max(left.maxDepth, right.maxDepth) + 1)
+
+# // Version 2: without ResultType
+# public class Solution {
+#     public boolean isBalanced(TreeNode root) {
+#         return maxDepth(root) != -1;
+#     }
+#
+#     private int maxDepth(TreeNode root) {
+#         if (root == null) {
+#             return 0;
+#         }
+#
+#         int left = maxDepth(root.left);
+#         int right = maxDepth(root.right);
+#         if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
+#             return -1;
+#         }
+#         return Math.max(left, right) + 1;
+#     }
+# }
